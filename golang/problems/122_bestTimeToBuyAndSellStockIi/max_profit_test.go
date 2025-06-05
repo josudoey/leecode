33f067c94913ed1d34d4842a81d00e094327961a
Joey Fang <josudoey@gmail.com>
2025-06-05 14:05:29 +0800

    feat: add best-time-to-buy-and-sell-stock-ii

---
+++ golang/problems/122_bestTimeToBuyAndSellStockIi/max_profit_test.go
@@ -0,0 +1,31 @@
+package code
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = DescribeTable("maxProfit",
+	func(nums []int, expected int) {
+		Expect(maxProfit(nums)).To(Equal(expected))
+	},
+	Entry("Case 1",
+		[]int{7, 1, 5, 3, 6, 4},
+		7,
+	),
+	Entry("Case 2",
+		[]int{1, 2, 3, 4, 5},
+		4,
+	),
+	Entry("Case 3",
+		[]int{7, 6, 4, 3, 1},
+		0,
+	),
+)
+
+func TestCode(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Code")
+}
